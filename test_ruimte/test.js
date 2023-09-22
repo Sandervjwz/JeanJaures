b55a48d9d098dbca659532fbb6e538acf09e0084
SanderSoftTouch <sander.v@softtouch.be>
2023-09-22 15:43:59 +0200

    20230922_middag

---
+++ test_ruimte/test.js
@@ -0,0 +1,36 @@
+//document.body.innerHTML += "<p>js werkt</p>"
+var namenArray = ["test1", "test2", "test3"];
+var orders = document.getElementById("orders")
+var rijen = document.getElementById("rijen");
+
+window.onload = function (){
+    //document.body.innerHTML += "<p>js werkt</p>"
+    console.log("test", orders, namenArray)
+    for(var j = 0; j < namenArray.length; j++){
+        orders.innerHTML += `<div class="order">${j}</div>`
+        rijen.innerHTML += 
+        `<div class="rij flex">
+            <div class="order">${j}</div>
+            <div class="item_naam">${namenArray[j]}</div>
+            ${maakScoreSchema(j)}
+        </div>
+        `
+    }
+    /*for(var i = 0; i < orders.length; i++){
+        for(var j = 0; j < namenArray.length; j++){
+            orders[i].innerHTML += `<div class="order">${j}</div>`
+        }
+    }*/
+}
+
+function maakScoreSchema(index){
+    var resultaat = ''
+    for(var i = 0; i < namenArray.length; i++){
+        if(index == i){
+            resultaat += `<div class="item">x</div>`
+        } else {            
+            resultaat += `<div class="item">-</div>`
+        }
+    }
+    return resultaat
+}
