5b1ab1710b41bd5f713db1baf995c6b9555b10ba
SanderSoftTouch <sander.v@softtouch.be>
2023-10-25 13:10:49 +0200

    mondaynightvibes

--- test_ruimte/test.js
+++ test_ruimte/test.js
@@ -35,5 +35,92 @@ function maakScoreSchema(index){
     return resultaat
 }
 
+window.onclick = function (event) {
+    var myBox = document.getElementById('test1');
 
+    if (event.target.contains(myBox) && event.target !== myBox) {
+       console.log('You clicked outside the box!', event.target, event);
+    } else {
+        console.log('You clicked inside the box!');
+    }
+}
 
+// https://docs.google.com/spreadsheets/d/1WigEkjbs9IqhgCRG3Vl7HOzRQ4IRtsznDuafR9IWi2o/edit#gid=1454454143
+const sheetID = '1WigEkjbs9IqhgCRG3Vl7HOzRQ4IRtsznDuafR9IWi2o'
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`
+const sheetName = '2324_Jean_Jaurès';
+//let qu = 'select *'
+const query = encodeURIComponent('select *'); //encodeURIComponent(qu)
+const url = `${base}&sheet=${sheetName}&tq${query}`
+const data = [];
+document.addEventListener("DOMContentLoaded", init);
+
+const output = document.querySelector(".output")
+
+function init(){
+    //console.log(url, query, "wekrt");
+    fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+        //console.log(rep);
+        const jsData = JSON.parse(rep.substring(47).slice(0, -2))
+        console.log(jsData);
+        const colz = [];
+        first = true;
+        jsData.table.cols.forEach(heading => {
+            if(first){
+                first = false;
+                console.log(heading);
+                console.log("eerste")
+            } else {
+                if(heading.label != null && !first){
+                    console.log("tweede")
+                    colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
+                } 
+            } 
+            //console.log("colz", colz);
+        })
+        jsData.table.rows.forEach(main => {
+            //console.log("main", main);
+            const row = {};
+            colz.forEach((ele, ind) => {
+                //console.log(ele, ind);
+                row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
+                //console.log("row", row, "ele", ele, "ind", ind);
+            })
+            data.push(row);
+        })
+        maker(data);
+            console.log("data", data);
+    })
+}
+
+function maker(json){
+    const div = document.createElement('div');
+    div.style.display = 'grid';
+    output.append(div);
+    let first = true;
+    json.forEach(el => {
+        //console.log(el);
+        /*
+        console.log('el', el)
+        const keys = Object.keys(el);
+        div.style.gridTemplateColumns = `repeat (${keys.length}, 1fr)`
+        if(first){
+            first = false;
+            keys.forEach(heading => {
+                const ele = document.createElement('div');
+                ele.textContent = heading.toUpperCase();
+                ele.style.background = 'Black'
+                ele.style.color = 'white'
+                div.append(ele);
+            })
+        }
+        keys.forEach(key => {
+            const ele = document.createElement('div');
+            ele.textContent = el[key];
+            ele.style.border = '1px solid #ddd'
+            div.append(ele);
+        })*/
+    })
+}
