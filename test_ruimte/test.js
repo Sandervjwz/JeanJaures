5b00123da63864d87444ece98962f87db481d835
Sander <sander.v@softtouch.be>
2023-10-25 13:08:26 +0200

    testest

--- test_ruimte/test.js
+++ test_ruimte/test.js
@@ -16,6 +16,7 @@ window.onload = function (){
         </div>
         `
     }
+    //testspan.innerText = '';
     /*for(var i = 0; i < orders.length; i++){
         for(var j = 0; j < namenArray.length; j++){
             orders[i].innerHTML += `<div class="order">${j}</div>`
@@ -33,4 +34,32 @@ function maakScoreSchema(index){
         }
     }
     return resultaat
+}
+
+let timer;
+const waitTime = 1000;
+const testspan = document.getElementById('testspan');
+const inputvakje = document.getElementById('inputvakje');
+
+inputvakje.addEventListener('keyup', event => {
+  clearTimeout(timer);
+
+  timer = setTimeout(() => {
+    doneTyping(event.target.value);
+  }, waitTime);
+});
+
+function doneTyping(value) {
+    var te_testen = value.split("@").pop();
+    if(value.includes('@')){
+        if(te_testen.trim().toLowerCase() == 'softtouch.be'){
+            testspan.innerText = "Opgepast, ben je zeker dat je je @softtouch.be account wilt gebruiken?".toUpperCase();
+            //alert("Opgepast, ben je zeker dat je je @softtouch.be account wilt gebruiken?".toUpperCase());
+            testspan.style.color = "red";
+        } else {
+            testspan.innerText = "";
+        }
+    } else {
+        testspan.innerText = "";
+    } 
 }
